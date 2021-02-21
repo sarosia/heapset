5653d52955dfe34124300ab9b5b9b16669b3da19
Chi Ming To <tochiming@gmail.com>
2021-02-21 09:12:51 +0000

    Returns true when pushing an non-exist element into the heap, otherwise, return false.

--- test/heapset.js
+++ test/heapset.js
@@ -4,30 +4,36 @@ const HeapSet = require('..');
 describe('heapset', () => {
   it('No duplicates', () => {
     const heap = new HeapSet();
-    heap.push(2);
-    heap.push(3);
-    heap.push(1);
+    expect(heap.push(2)).to.be.equal(true);
+    expect(heap.push(3)).to.be.equal(true);
+    expect(heap.push(1)).to.be.equal(true);
 
     expect(heap.size()).to.be.equal(3);
     expect(heap.pop()).to.be.equal(1);
     expect(heap.pop()).to.be.equal(2);
     expect(heap.pop()).to.be.equal(3);
     expect(heap.size()).to.be.equal(0);
+
+    expect(heap.peek()).to.be.equal(undefined);
+    expect(heap.pop()).to.be.equal(undefined);
   });
 
   it('Duplicates', () => {
     const heap = new HeapSet();
-    heap.push(2);
-    heap.push(2);
-    heap.push(3);
-    heap.push(3);
-    heap.push(1);
-    heap.push(1);
+    expect(heap.push(2)).to.be.equal(true);
+    expect(heap.push(2)).to.be.equal(false);
+    expect(heap.push(3)).to.be.equal(true);
+    expect(heap.push(3)).to.be.equal(false);
+    expect(heap.push(1)).to.be.equal(true);
+    expect(heap.push(1)).to.be.equal(false);
 
     expect(heap.size()).to.be.equal(3);
     expect(heap.pop()).to.be.equal(1);
     expect(heap.pop()).to.be.equal(2);
     expect(heap.pop()).to.be.equal(3);
     expect(heap.size()).to.be.equal(0);
+
+    expect(heap.peek()).to.be.equal(undefined);
+    expect(heap.pop()).to.be.equal(undefined);
   });
 });
